959d5170224af871c31e259db4357a04306cce37
ErdenerDereci <43935534+ErdenerDereci@users.noreply.github.com>
2021-03-19 23:45:47 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product {
+                ProductId = 1,
+                CategoryId = 1,
+                ProductName = "Silgi",
+                UnitPrice = 15,
+                UnitsInStock= 5
+            };
+
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
